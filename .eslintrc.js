7b6b109cbc1c82b066c049226bc6f966fa60f9c3
Zineb LAHJOUJI <zineb.lahjouji@planete-oui.fr>
2021-12-11 17:33:29 +0100

    decomment ts rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -25,10 +25,10 @@ module.exports = {
     "extends": [
         "airbnb",
         "airbnb-typescript",
-        // "eslint:recommended",
-        // "plugin:@typescript-eslint/recommended",
-        // "plugin:react/recommended",
-        // "plugin:react-hooks/recommended",
+        "eslint:recommended",
+        "plugin:@typescript-eslint/recommended",
+        "plugin:react/recommended",
+        "plugin:react-hooks/recommended",
         "next/core-web-vitals",
         "plugin:storybook/recommended"
     ],
