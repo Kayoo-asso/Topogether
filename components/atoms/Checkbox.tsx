eb5a9d026b9df43e876e084b55b0cc8cef90677f
Zineb LAHJOUJI <zineb.lahjouji@planete-oui.fr>
2021-12-12 00:04:21 +0100

    Checkbox

--- components/atoms/Checkbox.tsx
+++ components/atoms/Checkbox.tsx
@@ -3,10 +3,31 @@ import { Icon } from './Icon';
 
 interface CheckboxProps {
   checked: boolean,
+  label?: string,
+  onClick: () => void
 }
 
 export const Checkbox: React.FC<CheckboxProps> = (props) => {
   const [isChecked, setIsChecked] = useState(props.checked);
   const icon = isChecked ? 'checked' : 'checkbox';
-  return (<Icon name={icon} onClick={() => setIsChecked(!isChecked)} />);
+  const iconColor = isChecked ? 'stroke-main' : 'stroke-dark';
+
+  const handleClick = () => {
+    setIsChecked(!isChecked);
+    props.onClick();
+  };
+
+  return (
+    <div className="flex items-center">
+      <Icon
+        className={`h-5 w-5 mr-2 ${iconColor}`}
+        name={icon}
+        onClick={handleClick}
+      />
+      <span className="ktext-base dark">
+        {props.label}
+
+      </span>
+    </div>
+  );
 };
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,9 +2,7 @@ import { Checkbox } from 'components/atoms/Checkbox';
 import type { NextPage } from 'next';
 
 const Map: NextPage = () => (
-  <div className="bg-dark">
-    <Checkbox checked={false} onClick={() => {}} />
-  </div>
+  <Checkbox checked={false} onClick={() => {}} label="text" />
 );
 
 export default Map;
