026db38439e0a24efa1fbf32b00869397f2a02c8
Zineb LAHJOUJI <zineb.lahjouji@planete-oui.fr>
2021-12-11 14:17:30 +0100

    Storybook integration

--- components/atoms/Button.stories.tsx
+++ components/atoms/Button.stories.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { ComponentStory, ComponentMeta } from '@storybook/react';
 
-import { Button } from './Button';
+import { Button } from './buttons/Button';
 
 // More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 export default {
@@ -19,23 +19,24 @@ const Template: ComponentStory<typeof Button> = (args) => <Button {...args} />;
 export const Primary = Template.bind({});
 // More on args: https://storybook.js.org/docs/react/writing-stories/args
 Primary.args = {
-  primary: true,
-  label: 'Button',
+  white: true,
+  content: 'Button',
 };
 
 export const Secondary = Template.bind({});
 Secondary.args = {
-  label: 'Button',
+  white: false,
+  content: 'Button',
 };
 
 export const Large = Template.bind({});
 Large.args = {
-  size: 'large',
-  label: 'Button',
+  fullWidth: true,
+  content: 'Button',
 };
 
 export const Small = Template.bind({});
 Small.args = {
-  size: 'small',
-  label: 'Button',
+  fullWidth: false,
+  content: 'Button',
 };
