75db0b306b97d6785aed594710661ae06a668026
Zineb LAHJOUJI <zineb.lahjouji@planete-oui.fr>
2021-12-13 10:14:56 +0100

    Select wip

--- components/molecules/form/Select.tsx
+++ components/molecules/form/Select.tsx
@@ -1,4 +1,7 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, {
+  useRef, useState,
+} from 'react';
+import { Icon } from '../../atoms/Icon';
 import { Dropdown } from './Dropdown';
 import { TextInput } from './TextInput';
 
@@ -11,25 +14,37 @@ interface SelectProps {
   id: string;
   label: string;
   choices: DropdownOption[];
-  selected: DropdownOption;
-  onSelect?: (value: any) => void;
+  selected: any;
+  onSelect: (value: any) => void;
 }
 
 export const Select: React.FC<SelectProps> = (props) => {
   const ref = useRef<HTMLInputElement>(null);
   const [isOpen, setIsOpen] = useState(false);
+  const selectedChoice = props.choices.find((choice) => choice.value === props.selected);
+  const [value, setValue] = useState(selectedChoice?.label);
 
   return (
-    <div id={props.id}>
+    <div id={props.id} onFocus={() => setIsOpen(true)} onBlur={() => setIsOpen(false)} className="relative">
       <TextInput
         ref={ref}
         label={props.label}
         id={`${props.id}-input`}
-        value={props.selected?.label}
-        onFocus={() => setIsOpen(true)}
-        onBlur={() => setIsOpen(false)}
+        value={value}
+        className="py-5 w-60"
       />
-      {isOpen && <Dropdown choices={props.choices} onSelect={props.onSelect} />}
+      <Icon name="arrow-simple" className="absolute -rotate-90 fill-dark w-5 h-5 top-2 left-22" />
+      {isOpen && (
+      <Dropdown choices={props.choices.map((choice) => ({
+        ...choice,
+        action: () => {
+          setValue(choice.label);
+          setIsOpen(false);
+          props.onSelect(choice.value);
+        },
+      }))}
+      />
+      )}
     </div>
   );
 };
