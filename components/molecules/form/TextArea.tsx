8319d25b2769e85b3482e6b847c322d916b50fbe
Zineb LAHJOUJI <zineb.lahjouji@planete-oui.fr>
2022-02-02 22:08:29 +0100

    auto resize textarea

--- components/molecules/form/TextArea.tsx
+++ components/molecules/form/TextArea.tsx
@@ -1,33 +1,51 @@
-import React, { InputHTMLAttributes, forwardRef } from 'react';
+import React, {
+ InputHTMLAttributes, useEffect, useRef, useState, useCallback,
+} from 'react';
 
 interface TextAreaProps extends InputHTMLAttributes<HTMLTextAreaElement> {
   id: string;
   label?: string;
   displayLabel?: boolean,
   error?: string;
-  type?: string;
   pointer?: boolean;
   className?: string;
 }
 
-export const TextArea = forwardRef<HTMLTextAreaElement, TextAreaProps>(({
-  type = 'text',
+export const TextArea = ({
   displayLabel = true,
   className = '',
   pointer = false,
   ...props
-}: TextAreaProps, ref) => (
-  <div className={`relative mt-3 w-full ${className}`}>
-    <textarea
-      {...props}
-      ref={ref}
-      placeholder={props.label}
-      id={props.id}
-      value={props.value}
-      className={`peer h-20 border-dark ktext-base border-b-2 ${displayLabel ? 'placeholder-transparent' : ''} focus:border-main focus:outline-none w-full ${pointer ? ' cursor-pointer' : ''}`}
-    />
+}: TextAreaProps) => {
+  const ref = useRef<HTMLTextAreaElement>(null);
 
-    {displayLabel && (
+  const [height, setHeight] = useState<number>(0);
+
+  const adaptScrollHeight = useCallback(() => {
+    if (ref?.current?.scrollHeight !== height) setHeight(0);
+  }, [height]);
+
+    useEffect(() => {
+    const refScrollHeight = ref?.current?.scrollHeight;
+      if (height === 0 && refScrollHeight) {
+        setHeight(refScrollHeight);
+      }
+  }, [height]);
+
+  return (
+    <div className={`relative mt-3 w-full ${className}`}>
+      <textarea
+        ref={ref}
+        {...props}
+        style={{ height: props.value ? `${height}px` : '40px' }}
+        placeholder={props.label}
+        id={props.id}
+        value={props.value}
+        className={`peer border-dark ktext-base border-b-2 overflow-hidden ${displayLabel ? 'placeholder-transparent' : ''} focus:border-main focus:outline-none w-full ${pointer ? ' cursor-pointer' : ''}`}
+        onKeyUp={adaptScrollHeight}
+      />
+
+      {displayLabel && (
       <label
         htmlFor={props.id}
         className={`absolute left-0 -top-6 ktext-label text-grey-medium peer-focus:text-main transition-all peer-placeholder-shown:top-2 peer-focus:-top-6 ${pointer ? ' cursor-pointer' : ''}`}
@@ -36,9 +54,10 @@ export const TextArea = forwardRef<HTMLTextAreaElement, TextAreaProps>(({
       </label>
     )}
 
-    {props.error
+      {props.error
       && <div className="ktext-error text-error">{props.error}</div>}
-  </div>
-));
+    </div>
+);
+};
 
 TextArea.displayName = 'TextInput';
