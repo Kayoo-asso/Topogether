2e3329ae47210d168375f2e1a3ee502f58231e08
Flavien <flavien@chervet.fr>
2022-04-01 18:03:28 +0200

    add loader to CFImage

--- components/atoms/CFImage.tsx
+++ components/atoms/CFImage.tsx
@@ -1,10 +1,11 @@
 import { Image } from "types"
 import { SourceSize, VariantWidths } from "helpers/variants";
 import { cloudflareUrl } from "helpers/cloudflareUrl";
-import { ImgHTMLAttributes } from "react";
+import { ImgHTMLAttributes, useState } from "react";
 import defaultKayoo from 'public/assets/img/Kayoo_defaut_image.png';
 import type { StaticImageData } from 'next/image';
 import { useDevice } from "helpers/hooks/useDevice";
+import { Loading } from "components/layouts";
 
 export type CFImageProps = RawImageAttributes & {
     alt: string,
@@ -22,9 +23,10 @@ type RawImageAttributes = Omit<
 export const CFImage: React.FC<CFImageProps> = ({ 
     image, sizeHint,
     defaultImage = defaultKayoo,
-    ...props }: CFImageProps) => {
-    
+    ...props 
+}: CFImageProps) => {
     const device = useDevice();
+    const [loading, setLoading] = useState(true);
 
     let src = defaultImage.src;
     let width = defaultImage.width;
@@ -51,14 +53,28 @@ export const CFImage: React.FC<CFImageProps> = ({
         placeholder = undefined;
     }
         
-    return <img
-        src={src}
-        width={width}
-        height={height}
-        srcSet={srcSet}
-        sizes={sizes}
-        {...props}
-        loading={props.loading ?? 'lazy'}
-        decoding={props.decoding ?? 'async'}
-    />;
+    return (
+        <div className='w-full h-full relative'>
+            <img
+                src={src}
+                width={width}
+                height={height}
+                srcSet={srcSet}
+                sizes={sizes}
+                {...props}
+                loading={props.loading ?? 'lazy'}
+                decoding={props.decoding ?? 'async'}
+                onLoad={() => {
+                    setLoading(false);
+                }}
+            />
+            {loading &&
+                <div className='bg-grey-light z-1000 absolute w-full h-full top-0'>
+                    <Loading 
+                        bgWhite={false}
+                    />
+                </div>
+            }
+        </div>
+    )
 }
--- components/layouts/Loading.tsx
+++ components/layouts/Loading.tsx
@@ -1,12 +1,20 @@
 import { Icon } from 'components';
 import React from 'react';
 
-export const Loading: React.FC = () => {
+interface LoadingProps {
+    SVGClassName?: string
+    bgWhite?: boolean,
+}
+
+export const Loading: React.FC<LoadingProps> = ({
+    SVGClassName = 'w-24 h-24',
+    bgWhite = true,
+}) => {
     return (
-        <div className='flex flex-col w-full h-full items-center justify-center bg-white'>
+        <div className={'flex flex-col w-full h-full items-center justify-center'+(bgWhite ? ' bg-white' : '')}>
             <Icon
                 name="spinner"
-                SVGClassName="stroke-main w-24 h-24 animate-spin m-2"
+                SVGClassName={"stroke-main animate-spin m-2 "+SVGClassName}
                 center
             />
         </div>
