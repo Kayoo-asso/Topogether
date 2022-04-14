5169cc25e1a4d1d3b238d9537dcc03813dde80f9
Erwin Kuhn <erwin.kuhn@protonmail.com>
2022-04-14 19:29:20 +0200

    Overhaul service worker (+ fix 2 minor things)

--- worker/sw.ts
+++ worker/sw.ts
@@ -2,7 +2,7 @@ declare let self: ServiceWorkerGlobalScope;
 
 import { precacheAndRoute } from 'workbox-precaching';
 import { registerRoute, Route } from 'workbox-routing';
-import { NetworkOnly, CacheFirst, StaleWhileRevalidate } from 'workbox-strategies';
+import { NetworkFirst, CacheFirst, StaleWhileRevalidate } from 'workbox-strategies';
 import { CacheExpiration, ExpirationPlugin } from 'workbox-expiration';
 
 (self as any).__WB_DISABLE_DEV_LOGS = true;
@@ -26,8 +26,20 @@ const images = new Route(({ request, sameOrigin, url }) => {
 
 // Only cache scripts from the same origin for now
 const scripts = new Route(({ request, sameOrigin }) => {
-    return request.destination !== "script" && sameOrigin;
+    return request.destination === "script" && sameOrigin;
 }, new CacheFirst({
+    cacheName: 'scripts',
+    plugins: [
+        // this should clear old entries over time
+        new ExpirationPlugin({
+            maxEntries: 50
+        })
+    ]
+}));
+
+const extScripts = new Route(({ request, sameOrigin }) => {
+    return request.destination === "script" && !sameOrigin;
+}, new StaleWhileRevalidate({
     cacheName: 'scripts',
     plugins: [
         // this should clear old entries over time
@@ -51,10 +63,47 @@ const styles = new Route(({ request }) => {
 const tiles = new Route(({ request, url }) => {
     return url.hostname === 'map.googleapis.com' && url.pathname.startsWith('/maps') && !!url.searchParams.get('pb')
 }, new StaleWhileRevalidate({
-    cacheName: 'tiles'
+    cacheName: 'tiles',
+    plugins: [
+        new ExpirationPlugin({
+            maxEntries: 50,
+            purgeOnQuotaError: true
+        })
+    ]
 }));
 
-// registerRoute(tiles);
+const documents = new Route(({ request }) => {
+    return request.destination === "document";
+}, new NetworkFirst({
+    cacheName: 'documents',
+    networkTimeoutSeconds: 7,
+    plugins: [
+        new ExpirationPlugin({
+            maxEntries: 30,
+            purgeOnQuotaError: true
+        })
+    ]
+}));
+
+const nextDataRegex = /^\/_next\/data\/.*\.json/;
+const nextData = new Route(({ url }) => {
+    return nextDataRegex.test(url.pathname); 
+}, new NetworkFirst({
+    cacheName: 'next_data',
+    networkTimeoutSeconds: 7,
+    plugins: [
+        new ExpirationPlugin({
+            maxEntries: 30,
+            purgeOnQuotaError: true
+        })
+    ]
+}));
+
+
 registerRoute(images);
 registerRoute(scripts);
+registerRoute(extScripts);
+registerRoute(tiles);
+registerRoute(documents);
 registerRoute(styles);
+registerRoute(nextData);
