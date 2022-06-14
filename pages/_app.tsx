4a29f4ca40e1dec84bfdab5837316804b9c49cb3
Flavien <flavien@chervet.fr>
2022-06-14 11:31:32 +0200

    test install button on android

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -69,14 +69,14 @@ const CustomApp = ({ Component, pageProps, session, initialDevice }: Props) => {
         <UserPositionProvider>
           <DeviceContext.Provider value={device}>
               <div ref={observe} className="w-screen h-screen flex items-end flex-col">
-                <div id={(device === 'mobile' && process.env.NODE_ENV !== 'development') ? "standalone" : ''} className='w-full h-full'>
+                {/* <div id={(device === 'mobile' && process.env.NODE_ENV !== 'development') ? "standalone" : ''} className='w-full h-full'>
                   <div id="content" className="flex-1 w-screen absolute bg-grey-light flex flex-col h-full md:h-screen overflow-hidden">
                     <Component {...pageProps} />
                   </div>
                   <div id="footer" className="bg-dark z-500 absolute bottom-0 h-shell md:hidden">
                     <ShellMobile />
                   </div>
-                </div>
+                </div> */}
 
                 {device === 'mobile' && process.env.NODE_ENV !== 'development' &&
                   <div id="no-standalone" className='z-full'><NoStandalone /></div>
