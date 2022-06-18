0c5f15d4980f1fe07d122243f646489399261b22
Flavien <flavien@chervet.fr>
2022-06-18 19:46:27 +0200

    test launchNavigation

--- helpers/map/launchNavigation.ts
+++ helpers/map/launchNavigation.ts
@@ -6,6 +6,7 @@ const launchNavigation = (location: GeoCoordinates, provider: 'apple' | 'google'
   const { lat, lng } = toLatLng(location);
   if (provider === 'apple') {
     navigator.geolocation.getCurrentPosition((pos) => {
+      alert(pos);
       device === 'desktop' ?
         window.open('https://maps.apple.com/?saddr=' + pos.coords.latitude + ',' + pos.coords.longitude + '&daddr=' + lat + ',' + lng + '&dirflg=d') :
         // TODO : fix Apple plan opening
@@ -14,6 +15,7 @@ const launchNavigation = (location: GeoCoordinates, provider: 'apple' | 'google'
   }
   else {
     navigator.geolocation.getCurrentPosition((pos) => {
+      alert(pos);
       device === 'desktop' ?
         window.open("https://www.google.com/maps/dir/?api=1&travelmode=driving&layer=traffic&destination=" + lat + "," + lng) :
         window.open("https://www.google.com/maps/dir/?api=1&origin=" + pos.coords.latitude + "," + pos.coords.longitude + "&destination=" + lat + "," + lng + "&travelmode=driving&layer=traffic");
