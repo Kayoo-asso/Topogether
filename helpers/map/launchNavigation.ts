b076a038fb1d00c2438a1d7109eb05b8b9ce534f
Flavien <flavien@chervet.fr>
2022-06-18 19:55:36 +0200

    test launchNavigation

--- helpers/map/launchNavigation.ts
+++ helpers/map/launchNavigation.ts
@@ -1,28 +1,25 @@
 import { Device } from "helpers/context";
-import { useIsIos } from "helpers/hooks";
 import { GeoCoordinates } from "types";
 import { toLatLng } from ".";
 
 const launchNavigation = (location: GeoCoordinates, provider: 'apple' | 'google', device: Device) => {
   const { lat, lng } = toLatLng(location);
-  const isIos = useIsIos();
-  alert("1");
+  
+  // OPEN ON PLAN
   if (provider === 'apple') {
     navigator.geolocation.getCurrentPosition((pos) => {
-      alert("2");
       device === 'desktop' ?
         window.open('https://maps.apple.com/?saddr=' + pos.coords.latitude + ',' + pos.coords.longitude + '&daddr=' + lat + ',' + lng + '&dirflg=d') :
         // TODO : fix Apple plan opening
         window.open('maps://?saddr=' + pos.coords.latitude + ',' + pos.coords.longitude + '&daddr=' + lat + ',' + lng + '&dirflg=d');
     });
   }
+
+  // OPEN ON GOOGLE MAP
   else {
-    navigator.geolocation.getCurrentPosition((pos) => {
-      device === 'desktop' ?
-        window.open("https://www.google.com/maps/dir/?api=1&travelmode=driving&layer=traffic&destination=" + lat + "," + lng) :
-        isIos ? window.open("https://maps.google.com/maps/dir/?api=1&origin=" + pos.coords.latitude + "," + pos.coords.longitude + "&destination=" + lat + "," + lng + "&travelmode=driving&layer=traffic") :
-          window.open("https://www.google.com/maps/dir/?api=1&origin=" + pos.coords.latitude + "," + pos.coords.longitude + "&destination=" + lat + "," + lng + "&travelmode=driving&layer=traffic");
-    });
+    device === 'desktop' ?
+      window.open("https://www.google.com/maps/dir/?api=1&travelmode=driving&layer=traffic&destination=" + lat + "," + lng) :
+      window.open("https://maps.google.com/maps/dir/?api=1&travelmode=driving&layer=traffic&destination=" + lat + "," + lng);
   }
 }
 
