369b7193cd62b97c4a211325970bec6ec2bfb88e
Flavien <flavien@chervet.fr>
2022-06-18 20:09:54 +0200

    test launchNavigation

--- helpers/map/launchNavigation.ts
+++ helpers/map/launchNavigation.ts
@@ -2,24 +2,37 @@ import { Device } from "helpers/context";
 import { GeoCoordinates } from "types";
 import { toLatLng } from ".";
 
-const launchNavigation = (location: GeoCoordinates, provider: 'apple' | 'google', device: Device) => {
-  const { lat, lng } = toLatLng(location);
-  
-  // OPEN ON PLAN
-  if (provider === 'apple') {
-    navigator.geolocation.getCurrentPosition((pos) => {
+const launchNavigation = (destination: GeoCoordinates, origin: GeoCoordinates | null, provider: 'apple' | 'google', device: Device) => {
+  const d = toLatLng(destination);
+
+  // We have the user position so we can propose the itinerary
+  if (origin) {
+    const o = toLatLng(origin);
+    if (provider === 'apple') { // OPEN ON PLAN
       device === 'desktop' ?
-        window.open('https://maps.apple.com/?saddr=' + pos.coords.latitude + ',' + pos.coords.longitude + '&daddr=' + lat + ',' + lng + '&dirflg=d') :
+        window.open('https://maps.apple.com/?saddr=' + o.lat + ',' + o.lng+ '&daddr=' + d.lat + ',' + d.lng + '&dirflg=d') :
         // TODO : fix Apple plan opening
-        window.open('maps://?saddr=' + pos.coords.latitude + ',' + pos.coords.longitude + '&daddr=' + lat + ',' + lng + '&dirflg=d');
-    });
+        window.open('maps://?saddr=' + o.lat + ',' + o.lng + '&daddr=' + d.lat + ',' + d.lng + '&dirflg=d');
+    }
+    else { // OPEN ON GOOGLE MAP
+      device === 'desktop' ?
+        window.open("https://www.google.com/maps/dir/?api=1&travelmode=driving&layer=traffic&destination=" + d.lat + "," + d.lng) :
+        window.open("https://maps.google.com/maps/dir/?api=1&travelmode=driving&layer=traffic&destination=" + d.lat + "," + d.lng);
+    }
   }
 
-  // OPEN ON GOOGLE MAP
   else {
-    device === 'desktop' ?
-      window.open("https://www.google.com/maps/dir/?api=1&travelmode=driving&layer=traffic&destination=" + lat + "," + lng) :
-      window.open("https://maps.google.com/maps/dir/?api=1&travelmode=driving&layer=traffic&destination=" + lat + "," + lng);
+    if (provider === 'apple') { // OPEN ON PLAN
+      device === 'desktop' ?
+        window.open('https://maps.apple.com/?daddr=' + d.lat + ',' + d.lng + '&dirflg=d') :
+        // TODO : fix Apple plan opening
+        window.open('maps://?daddr=' + d.lat + ',' + d.lng + '&dirflg=d');
+    }
+    else { // OPEN ON GOOGLE MAP
+      device === 'desktop' ?
+        window.open("https://www.google.com/maps/dir/?api=1&travelmode=driving&layer=traffic&destination=" + d.lat + "," + d.lng) :
+        window.open("https://maps.google.com/maps/dir/?api=1&travelmode=driving&layer=traffic&destination=" + d.lat + "," + d.lng);
+    }
   }
 }
 
