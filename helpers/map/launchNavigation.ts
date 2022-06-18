7002d2ab5d17d00305ad86a6ac8af40d9ea92b10
Flavien <flavien@chervet.fr>
2022-06-18 19:49:44 +0200

    test launchNavigation

--- helpers/map/launchNavigation.ts
+++ helpers/map/launchNavigation.ts
@@ -1,24 +1,27 @@
 import { Device } from "helpers/context";
+import { useIsIos } from "helpers/hooks";
 import { GeoCoordinates } from "types";
 import { toLatLng } from ".";
 
 const launchNavigation = (location: GeoCoordinates, provider: 'apple' | 'google', device: Device) => {
   const { lat, lng } = toLatLng(location);
+  const isIos = useIsIos();
+  alert("1");
   if (provider === 'apple') {
     navigator.geolocation.getCurrentPosition((pos) => {
-      alert(pos);
+      alert("2");
       device === 'desktop' ?
         window.open('https://maps.apple.com/?saddr=' + pos.coords.latitude + ',' + pos.coords.longitude + '&daddr=' + lat + ',' + lng + '&dirflg=d') :
         // TODO : fix Apple plan opening
-        window.open('maps://maps.google.com/maps?saddr=' + pos.coords.latitude + ',' + pos.coords.longitude + '&daddr=' + lat + ',' + lng + '&dirflg=d');
+        window.open('maps://?saddr=' + pos.coords.latitude + ',' + pos.coords.longitude + '&daddr=' + lat + ',' + lng + '&dirflg=d');
     });
   }
   else {
     navigator.geolocation.getCurrentPosition((pos) => {
-      alert(pos);
       device === 'desktop' ?
         window.open("https://www.google.com/maps/dir/?api=1&travelmode=driving&layer=traffic&destination=" + lat + "," + lng) :
-        window.open("https://www.google.com/maps/dir/?api=1&origin=" + pos.coords.latitude + "," + pos.coords.longitude + "&destination=" + lat + "," + lng + "&travelmode=driving&layer=traffic");
+        isIos ? window.open("https://maps.google.com/maps/dir/?api=1&origin=" + pos.coords.latitude + "," + pos.coords.longitude + "&destination=" + lat + "," + lng + "&travelmode=driving&layer=traffic") :
+          window.open("https://www.google.com/maps/dir/?api=1&origin=" + pos.coords.latitude + "," + pos.coords.longitude + "&destination=" + lat + "," + lng + "&travelmode=driving&layer=traffic");
     });
   }
 }
