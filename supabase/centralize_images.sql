3c092e4f649e94ad5ca6a6be36a93a1a6a0ca437
Erwin Kuhn <erwin.kuhn@protonmail.com>
2022-07-30 12:27:16 +0200

    WIP: centralize_images.sql

--- supabase/centralize_images.sql
+++ supabase/centralize_images.sql
@@ -86,11 +86,92 @@ create table public.boulder_images (
 /* 5. Update dependent views */
 
 /* 5.a. public.profiles */
+-- Cascades to `light_topo` but we'll recreate that later anyways
+drop view public.profiles cascade;
+
+create view public.profiles as
+	select 
+		id,
+		"userName",
+		role,
+		created,
+		image_id,
+		"firstName",
+		"lastName",
+		country,
+		city
+	from public.accounts;
+
+create trigger profiles_are_read_only
+	instead of insert or update or delete 
+	on public.profiles
+	for each row execute function internal.do_nothing();
 
 /* 5.b. public.topos_with_like */
+drop view topos_with_like cascade;
+
+create view topos_with_like as
+    select t.*, likes_topo(t.id) as liked
+    from public.topos as t;
 
 /* 5.c. public.light_topos */
 
+/* DETAIL:  drop cascades to function build_light_topo(topos)
+drop cascades to view light_topos
+drop cascades to function search_light_topos(text,integer,double precision)
+drop cascades to function liked_topos_of_user(uuid)
+drop cascades to function get_contributor_topos(uuid) */
+drop type public.light_topo cascade;
 
+-- Make those 2 functions stable, to enable PostgreSQL to inline them
+create or replace function likes_topo(_topo_id uuid)
+returns boolean
+as $$
+    select exists(
+        select 1 from public.topo_likes
+        where "topoId" = _topo_id
+        and "userId" = auth.uid()
+    )
+$$ language sql stable;
+
+create or replace function likes_boulder(_boulder_id uuid)
+returns boolean
+as $$
+    select exists(
+        select 1 from public.boulder_likes
+        where "boulderId" = _boulder_id
+        and "userId" = auth.uid()
+    )
+$$ language sql volatile;
+
+create view public.light_topos as
+	select
+		t.id, t.name, t.status,
+		likes_topo(t.id) as liked,
+		image,
+		creator,
+		count(s) as "nbSectors",
+		count(b) as "nbBoulders",
+		count(tr) as "nbTracks",
+		(select location::jsonb->'coordinates' 
+		 from public.parkings 
+		 where "topoId" = t.id 
+		 limit 1
+		) as "parkingLocation"
+		-- (select jsonb_object_agg(grade, count)
+    --  from (
+    --     select grade_to_category(tr.grade) as grade, count(*) as count
+    --     from tr
+    --     group by grade_to_category(tr.grade)
+    --  ) as _
+		-- ).* as grades
+	from public.topos t
+	left join public.sectors s on s."topoId" = t.id
+	left join public.boulders b on b."topoId" = t.id
+	left join public.tracks tr on tr."topoId" = t.id
+	left join public.images image on image.id = t.image_id
+	left join public.profiles creator on creator.id = topo."creatorId"
+	-- lateral ( select grade_to_category(tr.grade) as grade, count(*) from tr group by grade_to_category(tr.grade) )
+	group by t.id;
 
 commit;
