07dd912e616ab745dabbb1183e49246df2b7245c
fudu69 <flavien@chervet.fr>
2022-08-29 12:05:13 +0200

    fix Accesses

--- components/organisms/form/AccessForm.tsx
+++ components/organisms/form/AccessForm.tsx
@@ -12,135 +12,137 @@ interface AccessFormProps {
 
 export const AccessForm: React.FC<AccessFormProps> = watchDependencies(
 	(props: AccessFormProps) => {
-		if (props.accesses.length < 1) {
-			return (
-				<div className="w-full pt-[10%]" onClick={(e) => e.stopPropagation()}>
-					<Button
-						content="Ajouter une marche d'approche"
-						fullWidth
-						onClick={() => {
-							props.accesses.push({
-								id: v4(),
-								steps: [],
-							})
-						}}
-					/>
-				</div>
-			);
-		} else {
-			const accessQuark = props.accesses.quarkAt(0);
-			const access = accessQuark();
-			return (
-				<>
-					<div
-						className={`flex flex-col max-h-[90%] gap-6 pb-6 overflow-scroll ${
-							props.className ? props.className : ""
-						}`}
-						onClick={(e) => e.stopPropagation()}
-					>
-						<div className="flex w-full ktext-subtitle mb-1">Marche d'approche</div>
-						<div className="flex flex-row items-end gap-6">
-							<Select
-								id="access-difficulty"
-								label="Difficulté"
-								options={selectOptions(DifficultyName)}
-								value={access.difficulty}
-								onChange={useCallback((value: Difficulty) => {
-									accessQuark.set(a => ({
-										...a,
-										difficulty: value,
-									}));
-								}, [accessQuark])}
-							/>
-							<TextInput
-								id="access-duration"
-								label="Durée (min)"
-								type="number"
-								step={1}
-								value={access.duration}
-								onChange={useCallback((e) =>
-									accessQuark.set(a => ({
-										...a,
-										duration: parseInt(e.target.value),
-									}))
-								, [accessQuark])}
-							/>
-						</div>
+		const accessQuark = props.accesses.quarkAt(0);
+		const access = accessQuark ? accessQuark() : undefined;
+		
+		return (
+			<>
+				{props.accesses.length < 1 &&	
+					<div className="w-full pt-[10%]" onClick={(e) => e.stopPropagation()}>
+						<Button
+							content="Ajouter une marche d'approche"
+							fullWidth
+							onClick={() => {
+								props.accesses.push({
+									id: v4(),
+									steps: [],
+								})
+							}}
+						/>
+					</div>
+				}
+				{props.accesses.length > 0 && accessQuark && access &&
+					<>
+						<div
+							className={`flex flex-col max-h-[90%] gap-6 pb-6 overflow-scroll ${
+								props.className ? props.className : ""
+							}`}
+							onClick={(e) => e.stopPropagation()}
+						>
+							<div className="flex w-full ktext-subtitle mb-1">Marche d'approche</div>
+							<div className="flex flex-row items-end gap-6">
+								<Select
+									id="access-difficulty"
+									label="Difficulté"
+									options={selectOptions(DifficultyName)}
+									value={access.difficulty}
+									onChange={(value: Difficulty) => {
+										accessQuark.set(a => ({
+											...a,
+											difficulty: value,
+										}));
+									}}
+								/>
+								<TextInput
+									id="access-duration"
+									label="Durée (min)"
+									type="number"
+									step={1}
+									value={access.duration}
+									onChange={(e) =>
+										accessQuark.set(a => ({
+											...a,
+											duration: parseInt(e.target.value),
+										}))
+									}
+								/>
+							</div>
 
-						<div className="ktext-subtitle mt-3">Etapes</div>
-						<div className="flex flex-col gap-6">
-							{/* TODO : scroll to the new step when it is created */}
-							{access.steps?.map((step, index) => {
-								const newSteps = access.steps!;
-								return (
-									<div className="flex flex-col gap-2" key={index}>
-										<div
-											className="ktext-base-little mt-3 cursor-pointer text-main"
-											onClick={useCallback(() => {
-												newSteps.splice(index, 1);
-												accessQuark.set(a => ({
-													...a,
-													steps: newSteps,
-												}));
-											}, [newSteps, accessQuark])}
-										>
-											Supprimer l'étape
-										</div>
-										<div className="flex flex-row items-start gap-6">
-											<div className="w-32">
-												<ImageInput
-													value={step.image}
-													onChange={useCallback((images) => {
-														newSteps[index].image = images[0];
-														accessQuark.set(a => ({
-															...a,
-															steps: newSteps,
-														}));
-													}, [newSteps[index].image, accessQuark])}
-												/>
-											</div>
-											<TextArea
-												id={"step" + index + "-description"}
-												label="Description"
-												value={step.description}
-												onChange={useCallback((e) => {
-													newSteps[index].description = e.target.value as Description;
-														accessQuark.set(a => ({
+							<div className="ktext-subtitle mt-3">Etapes</div>
+							<div className="flex flex-col gap-6">
+								{/* TODO : scroll to the new step when it is created */}
+								{access.steps?.map((step, index) => {
+									const newSteps = access.steps!;
+									return (
+										<div className="flex flex-col gap-2" key={index}>
+											<div
+												className="ktext-base-little mt-3 cursor-pointer text-main"
+												onClick={() => {
+													newSteps.splice(index, 1);
+													accessQuark.set(a => ({
 														...a,
 														steps: newSteps,
 													}));
-												}, [newSteps[index].description, accessQuark])}
-											/>
+												}}
+											>
+												Supprimer l'étape
+											</div>
+											<div className="flex flex-row items-start gap-6">
+												<div className="w-32">
+													<ImageInput
+														value={step.image}
+														onChange={(images) => {
+															newSteps[index].image = images[0];
+															accessQuark.set(a => ({
+																...a,
+																steps: newSteps,
+															}));
+														}}
+													/>
+												</div>
+												<TextArea
+													id={"step" + index + "-description"}
+													label="Description"
+													value={step.description}
+													onChange={(e) => {
+														newSteps[index].description = e.target.value as Description;
+															accessQuark.set(a => ({
+															...a,
+															steps: newSteps,
+														}));
+													}}
+												/>
+											</div>
 										</div>
-									</div>
-								);
-							})}
+									);
+								})}
+							</div>
+
+							<Button
+								content="Ajouter une étape"
+								onClick={() => {
+									const newSteps = access.steps || [];
+									newSteps.push({
+										description: "" as Description,
+									});
+									accessQuark.set(a => ({
+										...a,
+										steps: newSteps,
+									}));
+								}}
+							/>
+
 						</div>
 
 						<Button
-							content="Ajouter une étape"
-							onClick={useCallback(() => {
-								const newSteps = access.steps || [];
-								newSteps.push({
-									description: "" as Description,
-								});
-								accessQuark.set(a => ({
-									...a,
-									steps: newSteps,
-								}));
-							}, [access.steps, accessQuark])}
+							content="Supprimer"
+							fullWidth
+							onClick={() => props.accesses.removeQuark(accessQuark)}
 						/>
-
-					</div>
-
-					<Button
-						content="Supprimer"
-						fullWidth
-						onClick={useCallback(() => props.accesses.removeQuark(accessQuark), [props.accesses, accessQuark])}
-					/>
-				</>
-			);
-		}
+					</>
+				}
+			</>
+		)
 	}
 );
 
