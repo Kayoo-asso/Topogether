990e90b6c4202e4e637b266e057e1cf0084cd180
Erwin Kuhn <erwin.kuhn@protonmail.com>
2022-12-18 19:14:54 +0100

    implement Cluster

---
+++ components/openlayers/sources/Cluster.tsx
@@ -0,0 +1,53 @@
+import OLCluster from "ol/source/Cluster";
+import { InferOptions, createLifecycle } from "../createLifecycle";
+import { forwardRef, useEffect } from "react";
+import { useLayer, useSource } from "../contexts";
+import VectorSource from "ol/source/Vector";
+
+const useBehavior = createLifecycle(OLCluster, {
+	events: [
+		"addfeature",
+		"change",
+		"changefeature",
+		"clear",
+		"error",
+		"featuresloadend",
+		"featuresloaderror",
+		"featuresloadstart",
+		"propertychange",
+		"removefeature",
+	],
+	reactive: ["distance", "minDistance"],
+  reset: ["source"]
+});
+
+export type ClusterProps = React.PropsWithChildren<
+	Omit<InferOptions<typeof useBehavior>, "source">
+>;
+
+export const Cluster = forwardRef<OLCluster, ClusterProps>(
+	({ children, ...props }, ref) => {
+		const source = useSource();
+		if (source && !(source instanceof VectorSource)) {
+			throw new Error(
+				"<Cluster> component needs to be used inside a vector source."
+			);
+		}
+		const cluster = useBehavior(
+			{ ...props, source: source as VectorSource | undefined },
+			ref
+		);
+		const layer = useLayer();
+		useEffect(() => {
+			if (layer && cluster) {
+        layer.setSource(cluster);
+        return () => {
+          layer.setSource(cluster.getSource());
+          cluster.setSource(null);
+          cluster.dispose();
+        }
+			}
+		}, [layer, cluster]);
+		return <>{children}</>;
+	}
+);
