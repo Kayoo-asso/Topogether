b06edb4b320e76f3ebec9865d556e4709c0a59db
fudu69 <flavien@chervet.fr>
2023-01-01 12:55:24 +0100

    wip

--- components/map/markers/CreatingMarkersLayer.tsx
+++ components/map/markers/CreatingMarkersLayer.tsx
@@ -25,7 +25,6 @@ export const CreatingMarkersLayer: React.FC<CreatingMarkersLayerProps> = (props:
     
     return (
         <>
-
             <Select
                 layers={["creating"]}
                 onSelect={(e) => props.onCreate && props.onCreate(e.mapBrowserEvent)}
