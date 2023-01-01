4026d9ba4d692e1c12e679807eea6ab9959c391a
Erwin Kuhn <erwin.kuhn@protonmail.com>
2023-01-01 11:32:39 +0100

    cleanup

--- test/db/db.test.ts
+++
@@ -1,7 +0,0 @@
-import { loginFakeAdmin } from "helpers/fakeData/loginFakeAdmin";
-import { seedLocalDb } from "helpers/fakeData/seedLocalDb";
-
-test("Login fake admin + seed DB", async () => {
-	await loginFakeAdmin();
-	await seedLocalDb();
-});
--- test/utils.ts
+++
@@ -1,7 +0,0 @@
-export const a = null;
-//TODO : put back this
-// export function getConsoleErrorSpy() {
-//     return jest
-//         .spyOn(console, 'error')
-//         .mockImplementation(() => { });
-// }
