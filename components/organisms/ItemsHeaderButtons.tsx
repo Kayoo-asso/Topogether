adca3d6787151afd9309fb5bd902a4e185c12bea
fudu69 <flavien@chervet.fr>
2023-03-04 21:13:00 +0100

    integrate close button on Track

--- components/organisms/ItemsHeaderButtons.tsx
+++ components/organisms/ItemsHeaderButtons.tsx
@@ -4,7 +4,7 @@ import { CloseButton } from "components/atoms/buttons/CloseButton";
 import { LikeButton } from "components/atoms/buttons/LikeButton";
 import { DownloadButton } from "components/atoms/buttons/DownloadButton";
 
-type itemType = LightTopo | Boulder | Parking | Waypoint;
+type itemType = LightTopo | Boulder | Parking | Waypoint | Track;
 
 interface ItemsHeaderButtonsProps {
     item: itemType;
@@ -13,15 +13,16 @@ interface ItemsHeaderButtonsProps {
 
 export const isTopo = (item: itemType): item is LightTopo => (item as LightTopo).rockTypes !== undefined;
 export const isBoulder = (item: itemType): item is Boulder => (item as Boulder).isHighball !== undefined;
+export const isTrack = (item: itemType): item is Track => (item as Track).isTraverse !== undefined;
 
 export const ItemsHeaderButtons: React.FC<ItemsHeaderButtonsProps> = (props: ItemsHeaderButtonsProps) => {
 
     return (
-        <div className="absolute flex flex-row w-full px-2 mt-2 justify-between z-10">
+        <div className={`absolute flex flex-row w-full px-2 mt-2 z-10 ${isTrack(props.item) ? 'justify-end pr-6' : 'justify-between'}`}>
 
-            <div className={`${(isTopo(props.item) || isBoulder(props.item)) ? "px-6" : "px-4"} relative flex flex-row gap-5 justify-evenly items-center bg-white rounded-full md:cursor-pointer`}>
+            <div className={`${(isTopo(props.item) || isBoulder(props.item)) ? "px-6" : "px-4"} ${isTrack(props.item) ? 'hidden' : ''} relative flex flex-row gap-5 justify-evenly items-center bg-white rounded-full md:cursor-pointer`}>
                 {(isTopo(props.item) || isBoulder(props.item)) && <LikeButton liked={props.item.liked} />}
-                <ShareButton location={props.item.location} />
+                {!isTrack(props.item) && <ShareButton location={props.item.location} />}
                 {isTopo(props.item) && <DownloadButton topo={props.item} />}
             </div>
 
